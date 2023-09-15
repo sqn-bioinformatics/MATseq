45d980af8ae297b97b01d78c92c1cbe17f7a2db8
Tess Afanasyeva <t.afanasyeva@hpc.research.local>
2023-09-15 17:07:59 +0200

    feature: trained NN model

--- MATseq.sh
+++ MATseq.sh
@@ -1,18 +1,25 @@
-#!/bin/bash
+# #!/bin/bash
 
-read -n1 -p 'Do you want to update the data from Genomescan server?
-press Y for yes! ' checkdata
-case $checkdata in 
-	y|Y) echo''
-	python library/scripts/updates_data_from_genomescan.py
-	echo 'Genomescan update completed';;
-esac
-echo ''
+# read -n1 -p 'Do you want to update the data from Genomescan server?
+# press Y for yes! ' checkdata
+# case $checkdata in 
+# 	y|Y) echo''
+# 	python library/scripts/updates_data_from_genomescan.py
+# 	echo 'Genomescan update completed';;
+# esac
+# echo ''
+# # 
+# snakemake -s library/scripts/gunzip.snk -p --cores 4
 
-snakemake -s library/scripts/gunzip.snk -p --cores 4
 
-python library/scripts/removes_PCR_duplicates.py
+# python library/scripts/removes_PCR_duplicates.py
 
-snakemake -s library/scripts/fastqc.snk -p --cores 4
+python library/scripts/removes_PCR_duplicates_bruno.py
 
-snakemake -s library/scripts/maping.snk -p --cores 4 --resources load=2
+
+# snakemake -s library/scripts/compare_dudepers.snk -p --cores 4
+
+
+# snakemake -s library/scripts/fastqc.snk -p --cores 4
+
+# snakemake -s library/scripts/maping.snk -p --cores 4 --resources load=2
